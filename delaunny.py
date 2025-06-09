9ac69bce35f3d59c27a26a211c4b99e3213aaa31
eleftheriaa <esolomonide@gmail.com>
2025-06-09 17:57:02 +0300

    tried to delaunny with flipped triangles

---
+++ delaunny.py
@@ -0,0 +1,106 @@
+from vvrpywork.constants import Key, Mouse, Color
+from vvrpywork.scene import Scene2D
+from vvrpywork.shapes import (
+    Point2D, Line2D, Triangle2D, Circle2D, Rectangle2D,
+    PointSet2D, LineSet2D, Polygon2D
+)
+
+from random import random
+
+
+
+def findAdjacentTriangle(tris:dict[str,Triangle2D], p1:Point2D, p2:Point2D) -> tuple[str,Point2D]:
+    tri_adj_key = ""
+    opp_ver = None
+
+    # TASK 1:
+    #   - Find a triangle that contains p1-p2.
+    #   - Save its key in `tri_adj_key` and the remaining vertex in `opp_ver`.
+
+    for key in tris:
+        v1 = tris[key].getPoint1()
+        v2 = tris[key].getPoint2()
+        v3 = tris[key].getPoint3()
+
+        if (p1 == v1 and p2 == v2 ) or (p1 == v2 and p2 == v1 ) : 
+            tri_adj_key=key
+            opp_ver=v3
+            break
+        if (p1 == v1 and p2 == v3 ) or (p1 == v3 and p2 == v1 ) : 
+            tri_adj_key=key
+            opp_ver=v2
+            break
+        if (p1 == v3 and p2 == v2 ) or (p1 == v2 and p2 == v3 ) : 
+            tri_adj_key=key
+            opp_ver=v1
+            break
+        
+        
+    return tri_adj_key, opp_ver
+
+def isDelaunay(t:Triangle2D, p:Point2D) -> bool:
+    '''Checks if `t` is a Delaunay triangle w.r.t `p`.'''
+
+    c = t.getCircumCircle()
+    c.radius *= 0.99  # Shrink the circle a bit in order to exclude points of its circumference.
+    if c.contains(p):
+        return False
+    return True
+
+def findViolations(all_triangles:dict[str,Triangle2D], new_triangle:Triangle2D) -> bool:
+    '''Checks if the given triangle is Delaunay.
+
+    Checks if a triangle is delaunay, checking all its adjacent
+    triangles.
+
+    Args:
+        all_triangles: A dictionary of all the triangles.
+        new_triangle: The triangle to check.
+    
+    Returns:
+        False if the given triangle is delaunay and True otherwise.
+    '''
+
+    is_delaunay = True
+    # 1. use findAdjacentTriangle() to check whether new_triangle is Delaunay
+    tri_adj_key, opp_ver = findAdjacentTriangle(all_triangles,new_triangle.getPoint1(),new_triangle.getPoint2())
+    if tri_adj_key:
+        if not isDelaunay(new_triangle, opp_ver): 
+            is_delaunay = False
+    tri_adj_key, opp_ver = findAdjacentTriangle(all_triangles,new_triangle.getPoint3(),new_triangle.getPoint2())
+    if tri_adj_key:
+        if not isDelaunay(new_triangle, opp_ver):
+            is_delaunay = False
+
+    tri_adj_key, opp_ver = findAdjacentTriangle(all_triangles,new_triangle.getPoint1(),new_triangle.getPoint3())
+    if tri_adj_key:
+        if not isDelaunay(new_triangle, opp_ver):
+            is_delaunay = False
+
+    # 2. use function isDelaunay()
+
+
+
+    return not is_delaunay
+
+
+def if_not_delaunny(triangle, all_triangles) -> list[str, Triangle2D, str, Triangle2D]:
+    # Get triangle vertices
+    p1, p2, p3 = triangle.getPoint1(), triangle.getPoint2(), triangle.getPoint3()
+    edges = [(p1, p2), (p2, p3), (p3, p1)]
+    third_vertices = [p3, p1, p2]  # Opposite vertex to each edge
+
+    for i, edge in enumerate(edges):
+        third_v = third_vertices[i]
+        adj_key, opp_vertex = findAdjacentTriangle(all_triangles, edge[0],edge[1])
+        if adj_key is None or opp_vertex is None:
+            continue  # Skip if no adjacent triangle found
+
+        # Flip edge: create two new triangles with the opposite point
+        new_tri1 = Triangle2D(opp_vertex, edge[0], third_v, color=Color.RED)
+        new_tri2 = Triangle2D(opp_vertex, edge[1], third_v, color=Color.RED)
+
+        name1 = str(random())
+        name2 = str(random())
+
+    return name1, new_tri1, name2, new_tri2  # Return first valid flip
--- test_terrain.py
+++ test_terrain.py
@@ -1,26 +1,35 @@
 import cv2
 import numpy as np
 from vvrpywork.constants import Key, Mouse, Color
-from vvrpywork.scene import Scene2D
-from vvrpywork.shapes import Point2D, PointSet2D, LineSet2D,Polygon2D, Line2D, Triangle2D
+from vvrpywork.scene import Scene2D, Scene3D
+from vvrpywork.shapes import Point2D, PointSet2D, LineSet2D,Polygon2D, Line2D, Triangle2D, PointSet3D,LineSet3D,Line3D
 from contour_map_function import generate_contour_function, generate_contour_image
 import matplotlib.pyplot as plt
 from skimage.morphology import skeletonize
 from random import random
+from delaunny import findViolations, if_not_delaunny
+from scipy.spatial import Delaunay
 
-N =50
+N =40
 WIDTH = 950
-HEIGHT = 940
+HEIGHT = 950
 COLORS = [Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.ORANGE, Color.MAGENTA, Color.YELLOWGREEN, Color.CYAN]
-class Terrain(Scene2D):
+H_STEP = 0.1
+S = Scene2D
+class Terrain(S):
+    
     def __init__(self):
         super().__init__(WIDTH, HEIGHT, "Terrain")
         self.triangles:dict[str, Triangle2D] = {}
-        self.sparse_pointlist = []
-    #     self.reset()
+        self.per_contour_triangles = {}
+        self.names = []
+        # self.all_triangles = []
+        # self.all_triangles_keys = []
+        
+        self.pointlist2d = []
+        self.new_triangles:dict[str, Triangle2D] = {}
+        self.filled_tris:dict[str, Triangle2D] = {} 
 
-    # def reset(self):
-    #     self.task2()
 
     def on_key_press(self, symbol, modifiers):
         if symbol == Key._1:
@@ -28,7 +37,25 @@ class Terrain(Scene2D):
 
         if symbol == Key._2:
             self.task2()
-   
+        
+        if symbol == Key.D:
+            self.check_delaunay_status()
+        if symbol == Key._3:
+            self.task2a()
+        if symbol == Key.R :
+            
+            for k in self.names:
+                self.removeShape(k)
+            self.names = []
+            for key in self.triangles.keys():
+                self.removeShape(key)
+            self.triangles = {}
+            for key,value in self.filled_tris.items():
+                value.filled=False
+                self.removeShape(key)
+            self.filled_tris = {}
+
+
 
     def resample_contour(self, contour, num_points):
         """Uniformly resample contour to have exactly `num_points` points."""
@@ -85,8 +112,6 @@ class Terrain(Scene2D):
         # cv2.CHAIN_APPROX_SIMPLE: stores essential points (like corners)
         contours, _ = cv2.findContours(skeleton_uint8, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE) 
         half_contours = contours[::2]
-        # names = []
-        count = 0
         count_points = 0
         # *************** DRAW IMAGE *********************
         # Image shape for normalization
@@ -100,15 +125,16 @@ class Terrain(Scene2D):
         print(f"sorted contours:{len(sorted_contours)} ")
         print(f"contours:{len(half_contours)} ")
 
-        pointlist2d = []
         # *************** DRAW CONTOURS *****************
+        pointlist2d = []
+        self.pointlist2d =pointlist2d
         for i, c in enumerate(half_contours):
             pointset = PointSet2D(size=0.5,color=Color.RED)
             lineset = LineSet2D(width = 0.5)
             nx_minus1, ny_minus1 = None, None
             # *************** RESAMPLE CONTOURS *****************
             rcontour = self.resample_contour(c, N)
-            print(f"contour_{count}", rcontour)
+            print(f"contour_{i}", rcontour)
             # print(rcontour)
             for x_pixel, y_pixel in rcontour:
                 # Normalize pixel coords to [-1, 1]
@@ -122,12 +148,13 @@ class Terrain(Scene2D):
                     lineset.add(Line2D(pn,pn_minus1,color=Color.BLACK))
                 nx_minus1, ny_minus1 = nx, ny
                 count_points +=1 
-            name = f"contour_{count}"
+            self.names.append(f"contour_{i}")
+
             self.addShape(lineset)
-            self.addShape(pointset, name)
+            self.addShape(pointset, self.names[i])
             pointlist2d.append(pointset)
-            count += 1
-        print("number of points in all contour",pointlist2d)   
+            # count += 1
+        # print("number of points in all contour",pointlist2d)   
         return pointlist2d
     
     def closest_point_in_contour(self,point, A):
@@ -152,10 +179,11 @@ class Terrain(Scene2D):
         K = len(A.points)
         L = len(B.points)
         step = 2
-        print(step)
-        
+        # print(step)
+        inner_tr_dict = {}
+
         for i in range(0, L, step):
-            print(i)
+            # print(i)
             b_curr = B[i]
             b_next = B[(i + 1) % L]
             b_next_next = B[(i + 2) % L]
@@ -164,40 +192,150 @@ class Terrain(Scene2D):
                 a_start = a_idx
             a_curr = A[(a_idx) % K]
             a_next = A[(a_idx + 1) % K]
+            a_next_next = A[(a_idx + 2) % K]
+
             # Form 3 triangles: (a_curr, a_next, b_curr) and (b_curr, b_next, a_next)
             new_tri1 = Triangle2D(b_curr, b_next, a_curr,width=0.5,color = Color.BLUE)
             new_tri2 = Triangle2D(a_curr, a_next, b_next,width=0.5,color = Color.BLUE)
 
             new_tri3 = Triangle2D(b_next_next, b_next, a_next,width=0.5,color = Color.BLUE)
+            new_tri4 = Triangle2D( a_next, a_next_next,b_next_next, width=0.5,color = Color.GREEN)
 
-        
-            name1 = str(random())
-            name2 = str(random())
-            name3 = str(random())
+            for tri in [new_tri1, new_tri2, new_tri3, new_tri4]:
+                name = str(random())
+                inner_tr_dict[name] = tri
+                self.triangles[name] = tri
+                self.addShape(tri, name)
 
-            self.triangles[name1] = new_tri1
-            self.triangles[name2] = new_tri2
-            self.triangles[name3] = new_tri3
-
-            self.addShape(new_tri1, name1)
-            self.addShape(new_tri3, name2)
-            self.addShape(new_tri2, name3)
             
             a_idx +=2
-            # if (a_idx == a_start+K ): break
+        contour_id = f"contour{len(self.per_contour_triangles) + 1}"
+        self.per_contour_triangles[contour_id] = inner_tr_dict
+        # self.all_triangles.append(inner_tr)
+        # self.all_triangles_keys.append(inner_tr_key)
 
-        return self.triangles
-   
-    
+    def check_delaunay_status(self) :
+        if (self.triangles == {}):
+            triangles = self.new_triangles
+        else: triangles = self.triangles
+        # Optionally reset if you're using self.names to store last-inserted triangle names
+        triangle_keys = list(triangles.keys())
+        c=0
+        for i, key in enumerate(triangle_keys):
+            triangle = triangles[key]
+            is_delaunay = not findViolations(triangles, triangle)
+
+            if (is_delaunay == True ): c +=1
+            else:
+                circle = triangle.getCircumCircle()
+                filled = Triangle2D(triangle.getPoint1(), triangle.getPoint2(), triangle.getPoint3(), color=Color.ORANGE, filled=True)
+                name_filled = str(random()) 
+                self.filled_tris[name_filled] = filled
+                self.addShape(filled)
+
+    def make_delaunny(self):
+        for i,contour_dicts in enumerate(self.per_contour_triangles.values()):
+            triangle_keys = list(contour_dicts.keys())
+            triangle_keys_next = np.concatenate((triangle_keys , list(self.per_contour_triangles[f"contour{i+1}"].keys())) )
+            triangle_values = list(contour_dicts.values())
+            triangle_values_next = np.concatenate((triangle_values , list(self.per_contour_triangles[f"contour{i+1}"].values())) )
+            curr_next_triangles = dict(zip(triangle_keys_next,triangle_values_next))
+            # print(curr_next_triangles)
+
+            c=0
+            for i, key in enumerate(triangle_keys):             
+                triangle = self.triangles[key]
+                is_delaunny = not findViolations(self.triangles, triangle)
+                if(is_delaunny): 
+                    self.new_triangles[key] = triangle 
+
+                    # self.addShape(triangle)
+                else: 
+                    new_key1, new_tri1, new_key2, new_tri2 = if_not_delaunny(triangle, curr_next_triangles)
+                    self.new_triangles[new_key1] = new_tri1
+                    self.new_triangles[new_key2] = new_tri2
+                    
+                    # self.addShape(new_tri1)
+                    # self.addShape(new_tri2)
+        for key in self.triangles.keys():
+            self.removeShape(key)
+        self.triangles = {}
+        for keys, new_triangle in self.new_triangles.items():
+            # print("new triangle ", new_triangle)
+            self.addShape(new_triangle,keys)
+        print("end of loop")
+
+
+    def peak(self, inner):
+        inner_num = len(inner.points)
+        x_coord = 0
+        y_coord = 0
+
+        # Compute centroid
+        for point2d in inner:
+            x_coord += point2d.x
+            y_coord += point2d.y
+        average = (x_coord / inner_num, y_coord / inner_num)
+        average_point = Point2D(average, color=Color.ORANGE, size=0.5)
+        self.addShape(average_point)
+
+        # Fan triangulation
+        for i in range(len(inner)):
+            point_current = inner[i]
+            point_next = inner[(i + 1) % len(inner)]  # wrap around to close the loop
+
+            tr = Triangle2D(point_current, point_next, average_point, width=0.3, color=Color.YELLOW)
+            name = str(random())
+            self.addShape(tr, name)
+
     def task2(self):
-
-        curve_points = self.task1()
-        print("size of contours:",len(curve_points))
         
+        curve_points = self.task1()
+        print("size of contours:",len(curve_points))  
+
         for i in range(len(curve_points)-1):
+            print(i)
+
+            if (i==0):
+                self.peak(curve_points[i])
             self.triangulate_between(curve_points[i],curve_points[i+1])
 
+        # self.check_delaunay_status()
 
+        # print("number of all triangles :", len(self.all_triangles))
+        
+        # return self.all_triangles
+    def task2a(self):
+        self.make_delaunny()
+    
+    def task2b(self):
+        all_triangles_2d = self.task2()
+        all_triangles_3d = []
+
+        for contour_index, contour_triangles in enumerate(all_triangles_2d):
+            z = contour_index * H_STEP
+            contour_3d = []
+
+            for i,tri in enumerate(contour_triangles):
+                if (i%2 == 0):
+                    h1 = 0
+                    h2 = 0.1
+                else:
+                    h1 = 0.1
+                    h2 = 0
+                point3d =((tri.x1, tri.y1, z + h2),
+                    (tri.x2, tri.y2, z + h2),
+                    (tri.x3, tri.y3, z + h1))
+                t3d = PointSet3D(point3d,size = 1, color = Color.BLACK)
+                triangle3d = LineSet3D(points=point3d,  width=1, color=Color.GREEN)
+                # self.addShape(triangle3d)
+                contour_3d.append(t3d)
+                self.addShape(t3d)
+                # self.addShape(triangle3d)
+
+            all_triangles_3d.append(contour_3d)
+
+        return all_triangles_3d
 
 
 
